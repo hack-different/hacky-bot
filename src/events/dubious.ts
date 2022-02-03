cb8b9bd0a84e96d17c9507319aa4f405df9ca97f
Rick Mark <rickmark@outlook.com>
2022-02-02 17:16:59 -0800

    Log messages in case

--- src/events/dubious.ts
+++ src/events/dubious.ts
@@ -8,6 +8,7 @@ class DubiousFilter {
     @On("messageCreate")
     private async onMessage([message]: ArgsOf<"messageCreate">, client: Client) {
         if (message.content.match(/jonathandata1/)) {
+            console.log(`deleting message from ${message.author.username}`)
             await message.delete()
             await message.author.send(`Yo, had to delete that message from ${message.channel}`)
         }
