42d56d9392cb5a8f1ca55101847a03d693bbdc5d
Rick Mark <rickmark@outlook.com>
2022-02-02 17:19:36 -0800

    enhancing logging

--- src/events/dubious.ts
+++ src/events/dubious.ts
@@ -8,7 +8,7 @@ class DubiousFilter {
     @On("messageCreate")
     private async onMessage([message]: ArgsOf<"messageCreate">, client: Client) {
         if (message.content.match(/jonathandata1/)) {
-            console.log(`deleting message from ${message.author.username}`)
+            console.log(`deleting message from ${message.author.username} in channel ${message.channel}`)
             await message.delete()
             await message.author.send(`Yo, had to delete that message from ${message.channel}`)
         }
